285466b3825fcd5a713ab7ff907746049a6b7190
TayniellyRosario <49009522+taynielly20@users.noreply.github.com>
2023-06-13 22:33:46 -0300

    Update atividademodelofull.cy.js

--- cypress/e2e/atividademodelofull.cy.js
+++ cypress/e2e/atividademodelofull.cy.js
@@ -5,13 +5,11 @@ describe('Tarefa', () => {
         cy.visit('')
     })
 
-    const nome = "meu nome"
-
     it('preencho meu nome', () => {
-        cy.get('input[name="name"]').type(nome);
+        cy.get('input[name="name"]').type("nome");
         cy.get('input[name="height"]').type("1,80");
         cy.get('input[name="weight"]').type("80");
         cy.get('button').click();
-        cy.get('div[class="user-info"]').should('be.visible',`Olá, ${nome}!`)
+        cy.get('div[class="user-info"]').should('be.visible').contains("nome");
     })
 })
