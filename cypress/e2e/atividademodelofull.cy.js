547c4704dab1c3821db152fcea28637fa399c480
TayniellyRosario <49009522+taynielly20@users.noreply.github.com>
2023-06-13 22:40:42 -0300

    Update atividademodelofull.cy.js

--- cypress/e2e/atividademodelofull.cy.js
+++ cypress/e2e/atividademodelofull.cy.js
@@ -6,10 +6,13 @@ describe('Tarefa', () => {
     })
 
     it('preencho meu nome', () => {
-        cy.get('input[name="name"]').type("nome");
+         cy.get('input[name="name"]').type("NOME");
         cy.get('input[name="height"]').type("1,80");
         cy.get('input[name="weight"]').type("80");
         cy.get('button').click();
-        cy.get('div[class="user-info"]').should('be.visible').contains("nome");
+        //cy.get('pre[class="user-name"]').contains("NOME");
+        cy.get('div[class="user-info"]').should('be.visible').contains("NOME"); // Valida se contem o NOME no retorno do calculo do IMC
+        //cy.get('span[class="user-result"]').should('be.visible').contains("Acima do peso");
+        //cy.get('span[class="user-result"]').contains("Acima do peso");
     })
 })
